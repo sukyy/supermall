d466f694f3d103000818062a8f5942f21caba7b2
yuexia <yue_xia@aliyun.com>
2020-03-19 22:14:30 +0800

    tabber

--- src/components/common/tabbar/TabBar.vue
+++ src/components/common/tabbar/TabBar.vue
@@ -1,59 +1,28 @@
 <template>
-    <div class="tab-bar">
-      <tab-bar-item>
-        <div slot="img">
-          <img src="assets/logo.png" class="img">
-        </div>
-        <div slot="title">
-          <span>首页</span>
-        </div>
-      </tab-bar-item>
-
-      <tab-bar-item>
-        <div slot="img">
-          <img src="https://i1.hdslb.com/bfs/face/c850c18ae6b507d0ef34837f53a51090b6a7451f.jpg@68w_68h.webp" class="img">
-        </div>
-        <div slot="title">
-          <span>首页</span>
-        </div>
-      </tab-bar-item>
-
-      <tab-bar-item>
-        <div slot="img">
-          <img src="https://i1.hdslb.com/bfs/face/c850c18ae6b507d0ef34837f53a51090b6a7451f.jpg@68w_68h.webp" class="img">
-        </div>
-        <div slot="title">
-          <span>首页</span>
-        </div>
-      </tab-bar-item>
-
-      <tab-bar-item>
-        <div slot="img">
-          <img src="https://i1.hdslb.com/bfs/face/c850c18ae6b507d0ef34837f53a51090b6a7451f.jpg@68w_68h.webp" class="img">
-        </div>
-        <div slot="title">
-          <span>首页</span>
-        </div>
-      </tab-bar-item>
-
-    </div>
-    
+  <div class="tab-bar">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-  import TabBarItem from "components/common/tabbar/TabBarItem"
-    export default {
-        name: "TabBar",
-      components:{
-        TabBarItem
-      }
-    }
+  export default {
+    name: "TabBar"
+  }
 </script>
 
-<style >
-  .img{
-    width: 40px;
-    height: 40px;
-  }
+<style scoped>
+  .tab-bar {
+    background-color: #f6f6f6;
+    height: 49px;
+    border-top: 1px solid #eee;
+    box-shadow: 0 -1px 1px rgba(150,150,150,.08);
 
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    display: flex;
+    text-align: center;
+  }
 </style>
